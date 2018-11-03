c18c9779709b77ed0bd35f67fa083e783bc932d8
tdreid <trevor.d.reid@gmail.com>
2018-11-03 19:15:52 +0000

    Specify timestamp format in test setup

--- blockchain.spec.js
+++ blockchain.spec.js
@@ -1,17 +1,39 @@
 const assert = require('assert');
 const Block = require('./block.js');
+const moment = require('moment');
 const Blockchain = require('./blockchain.js');
 
 let testchain = null;
 
 describe('The Blockchain', function() {
   beforeEach(function(done) {
+    this.timeout(5000);
     testchain = new Blockchain();
-    testchain.addBlock(new Block(1, new Date(), { test1: 1 }));
-    testchain.addBlock(new Block(2, new Date(), { test2: 2 }));
-    testchain.addBlock(new Block(3, new Date(), { test3: 3 }));
-    testchain.addBlock(new Block(4, new Date(), { testF: 'F' }));
-    testchain.addBlock(new Block(5, new Date(), { testF: 'F' }));
+    testchain.addBlock(
+      new Block(1, moment.utc().format('YYYY-MM-DD HH:mm:ss.SSSS'), {
+        test1: 1
+      })
+    );
+    testchain.addBlock(
+      new Block(2, moment.utc().format('YYYY-MM-DD HH:mm:ss.SSSS'), {
+        test2: 2
+      })
+    );
+    testchain.addBlock(
+      new Block(3, moment.utc().format('YYYY-MM-DD HH:mm:ss.SSSS'), {
+        test3: 3
+      })
+    );
+    testchain.addBlock(
+      new Block(4, moment.utc().format('YYYY-MM-DD HH:mm:ss.SSSS'), {
+        testF: 'F'
+      })
+    );
+    testchain.addBlock(
+      new Block(5, moment.utc().format('YYYY-MM-DD HH:mm:ss.SSSS'), {
+        testF: 'F'
+      })
+    );
     done();
   });
 
