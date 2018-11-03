c9f2873707b3084ab789da86e703e0a1c352be39
tdreid <trevor.d.reid@gmail.com>
2018-11-03 00:28:50 +0000

    Define a chain

---
+++ blockchain.js
@@ -0,0 +1,28 @@
+const Block = require('./block.js');
+
+class Blockchain
+{
+    constructor()
+    {
+        this.chain = [];
+    }
+    
+    initialize()
+    {
+        return new Block(0, '2018-11-01', [3,1,4,1,5,9,2,6,5,3,5], '3.1415926535');
+    }
+    
+    getLastBlock()
+    {
+        return this.chain[this.chain.length - 1];
+    }
+    
+    addBlock(block)
+    {
+        block.predecessorHash = this.getLastBlock().getHash();
+        block.hash = block.getHash();
+        this.chain.push(block);
+    }
+}
+
+module.exports = Blockchain;
