fb6cd31acfbec791b9efe51e53588141d8b65ab2
tdreid <trevor.d.reid@gmail.com>
2018-11-03 19:13:51 +0000

    Add mineBlock() method to block class

--- block.js
+++ block.js
@@ -15,6 +15,16 @@ class Block {
       this.index + this.timestamp + JSON.stringify(this.payload) + this.nonce
     ).toString();
   }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')
+    ) {
+      this.nonce++;
+      this.hash = this.getHash();
+    }
+    console.log(`BLOCK MINED: ${this.hash}`);
+  }
 }
 
 module.exports = Block;
